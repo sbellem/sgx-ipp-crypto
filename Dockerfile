8ad34277b12ed82b42094623fbc9ebf3766a29c2
Sylvain Bellemare <sbellem@gmail.com>
2021-06-13 00:52:52 -0400

    Add nix, docker and travis files

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# nixpkgs/nix:latest
+FROM nixpkgs/nix@sha256:c7ab99ed60cc587ac784742e2814303331283cca121507a1d4c0dd21ed1bdf83 as base
+
+WORKDIR /usr/src
+COPY nix nix
+
+FROM base as build
+ARG nix_derivation=sgxsdk.nix
+COPY ${nix_derivation} ${nix_derivation}
+RUN nix-build --quiet ${nix_derivation}
---
+++ ipp-nomitigation.nix
@@ -0,0 +1,56 @@
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }}:
+with pkgs;
+
+stdenvNoCC.mkDerivation {
+  name = "ippcrypto-nomitigation";
+  src = fetchFromGitHub {
+    owner = "sbellem";
+    repo = "linux-sgx";
+    rev = "68fed00c67532cf5be7df8dac5f9d13b3c25e9d8";
+    sha256 = "08a6y35s3rlvhcbd8zxqkw7d5vknb5b3mw0wa1wis6y7av5pskx2";
+    fetchSubmodules = true;
+  };
+  buildInputs = [
+    binutils
+    autoconf
+    automake
+    libtool
+    ocaml
+    ocamlPackages.ocamlbuild
+    file
+    cmake
+    gnum4
+    openssl
+    gcc
+    gnumake
+    texinfo
+    bison
+    flex
+    perl
+    python3
+    git
+    nasm
+  ];
+  dontConfigure = true;
+  # sgx expects binutils to be under /usr/local/bin by default
+  preBuild = ''
+    export BINUTILS_DIR=$binutils/bin
+    '';
+  buildPhase = ''
+    runHook preBuild
+
+    cd external/ippcp_internal/
+    make
+    
+    runHook postBuild
+    '';
+  postBuild = ''
+    mkdir -p $out
+    cp -r ./lib $out/lib
+    cp -r ./inc $out/inc
+    cp -r ./license $out/license
+    ls -l ./inc/
+  '';
+  dontInstall = true;
+  dontFixup = true;
+}
